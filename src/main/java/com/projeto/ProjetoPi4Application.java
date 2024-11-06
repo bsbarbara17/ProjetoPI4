9c7cae65eaa1ed861aae28928426eb82fbdf63c3
Barbara <Barbara@IdeaPadGaming3i>
2024-11-06 17:53:38 -0300

    first commit

---
+++ src/main/java/com/projeto/ProjetoPi4Application.java
@@ -0,0 +1,13 @@
+package com.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPi4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPi4Application.class, args);
+	}
+
+}
